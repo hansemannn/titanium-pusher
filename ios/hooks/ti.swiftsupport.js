b3509ca2e5a1193b36f76979d26dca01abf17fde
Hans Knöchel <h.knoechel@lambus.com>
2024-09-26 23:06:50 +0200

    feat: setup base SPM dependency

--- ios/hooks/ti.swiftsupport.js
+++ ios/hooks/ti.swiftsupport.js
@@ -1,6 +1,6 @@
 /**
  * Ti.SwiftSupport
- * Copyright (c) 2018-present by Axway Appcelerator
+ * Copyright TiDev, Inc. 04/07/2022-Present. All Rights Reserved.
  * All Rights Reserved.
  */
 
@@ -11,14 +11,117 @@ exports.cliVersion = '>=3.2';
 exports.init = init;
 
 /**
- * Main entry point for our plugin which looks for the platform specific
- * plugin to invoke
+ * A function to inject SPM packages into the Xcode project.
+ * The 24 char UUIDs should be random.
+ * Each UUID should be uppercase (by convention) and each of the 3 UUID params unique.
+ *
+ * @param {Object} xobjs - the Xcode plist object graph
+ * @param String swiftPackageFrameworkName Swift package framework name, e.g. MyPackage.
+ * @param String swiftPackageProductName Swift package product name, e.g. MyPackageKit
+ * @param String spmRemotePackageReference Swift package remote package reference name, e.g. MyPackage-iOS
+ * @param String spmRepositoryURL SPM repository URL, e.g. https://github.com/some-company/MyPackage-iOS
+ * @param String spmRepositoryVersionKind SPM repository kind, e.g. upToNextMajorVersion
+ * @param String spmRepositoryMinVersion SPM repository minimum version, e.g. 3.0.0
+ *
  */
-function init(logger, config, cli, appc) {
+function injectSPMPackage(xobjs,
+		swiftPackageFrameworkName, swiftPackageProductName,
+		spmRemotePackageReference,
+		spmRepositoryURL, spmRepositoryVersionKind, spmRepositoryMinVersion) {
+
+	const swiftProductUUID = '123';
+	const pbxBuildFileUUID = '111'
+	const spmRemotePackageUUID = '111'
+
+	// PBXBuildFile
+	xobjs.PBXBuildFile[pbxBuildFileUUID + " \/* " +
+		swiftPackageFrameworkName + " in Frameworks *\/"] = {
+			"isa":"PBXBuildFile",
+			"productRef":swiftProductUUID,
+			"fileRef_comment":swiftPackageProductName + " in Frameworks"
+		};
+
+	// PBXFrameworksBuildPhase
+	Object.keys(xobjs.PBXFrameworksBuildPhase).forEach(function (buildPhaseUUID) {
+		var buildPhase = xobjs.PBXFrameworksBuildPhase[buildPhaseUUID];
+		if (buildPhase && typeof buildPhase === 'object') {
+			buildPhase.files.push({
+				"value":pbxBuildFileUUID,
+				"comment":swiftPackageProductName + " in Frameworks"
+			});
+		}
+	});
+
+	// PBXNativeTarget
+	Object.keys(xobjs.PBXNativeTarget).forEach(function (nativeTargetUUID) {
+		var nativeTarget = xobjs.PBXNativeTarget[nativeTargetUUID];
+		if (nativeTarget && typeof nativeTarget === 'object') {
+			nativeTarget["packageProductDependencies"] =
+				"(\n\t\t\t\t" + swiftProductUUID + " \/* " +
+				swiftPackageProductName + " *\/,\n\t\t\t)";
+		}
+	});
+
+	// PBXProject
+	// xobjs.PBXProject
+	Object.keys(xobjs.PBXProject).forEach(function (pbxProjUUID) {
+		var pbxProj = xobjs.PBXProject[pbxProjUUID];
+		if (pbxProj && typeof pbxProj === 'object') {
+			pbxProj["packageReferences"] =
+				"(\n\t\t\t\t" + spmRemotePackageUUID +
+				" \/* XCRemoteSwiftPackageReference \"" +
+				spmRemotePackageReference + "\" *\/,\n\t\t\t)";
+
+		}
+	});
+
+	// XCRemoteSwiftPackageReference
+	const remoteSwiftPackageReferenceID = spmRemotePackageUUID +
+		" \/* XCRemoteSwiftPackageReference \"" +
+		spmRemotePackageReference + "\" *\/";
+
+	var xcRSPR = xobjs["XCRemoteSwiftPackageReference"] = {};
+	xcRSPR[remoteSwiftPackageReferenceID] =
+		{"isa":"XCRemoteSwiftPackageReference",
+		 "repositoryURL":"\"" + spmRepositoryURL + "\"",
+			"requirement":{"kind":spmRepositoryVersionKind,
+			"minimumVersion":spmRepositoryMinVersion}
+		}
+
+	// XCSwiftPackageProductDependency
+	const swiftPackageProductDependencyID = swiftProductUUID + " \/* " +
+			swiftPackageProductName + " *\/";
+	var xcSPPD = xobjs["XCSwiftPackageProductDependency"] = {};
+	xcSPPD[swiftPackageProductDependencyID] = {
+		"isa":"XCSwiftPackageProductDependency",
+		"package":spmRemotePackageUUID + " \/* XCRemoteSwiftPackageReference \"" +
+		spmRemotePackageReference + "\" *\/","productName":swiftPackageProductName
+	}
+
+}
+
+/**
+ * Main entry point for our plugin which looks for the platform specific
+ * plugin to invoke.
+ *
+ * @param {Object} logger The logger instance.
+ * @param {Object} config The hook config.
+ * @param {Object} cli The Titanium CLI instance.
+ * @param {Object} appc The Appcelerator CLI instance.
+ */
+// eslint-disable-next-line no-unused-vars
+function init(logger, config, cli) {
 	cli.on('build.ios.xcodeproject', {
-		pre: function(data) {
+		pre: function (data) {
+
 			var xobjs = data.args[0].hash.project.objects;
-															
+
+			injectSPMPackage(xobjs,
+				"PusherSwift", "PusherSwift",
+				"pusher-websocket-swift",
+				"https://github.com/pusher/pusher-websocket-swift.git", "upToNextMajorVersion", "10.1.5");
+
+			// PBXNativeTarget
 			Object.keys(xobjs.PBXNativeTarget).forEach(function (targetUuid) {
 				var target = xobjs.PBXNativeTarget[targetUuid];
 				if (target && typeof target === 'object') {
@@ -31,3 +134,4 @@ function init(logger, config, cli, appc) {
 		}
 	});
 }
+
